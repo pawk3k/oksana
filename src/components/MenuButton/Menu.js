7bb5ba0adbf194d0b49475d930bb1cc0e217948b
kraven <ravliv.pavlobohdan@gmail.com>
2020-07-27 16:15:35 +0200

    Git fix

--- src/components/MenuButton/Menu.js
+++ src/components/MenuButton/Menu.js
@@ -3,60 +3,6 @@ import {useDispatch, useSelector} from "react-redux";
 import "./Menu.css"
 import { IoIosClose } from "react-icons/io";
 
-import MenuButton from "./MenuButton";
-import {connect} from "react-redux";
-// class Menu extends Component{
-//
-//     dress = () => {
-//         this.props.dispatch({ type: 'DRESS' });
-//     };
-//     render() {
-//         let visibility = "hide";
-//
-//         if(this.props.menuVisibility){
-//             visibility = "show";
-//         }
-//
-//         return (
-//
-//             <div id="flyoutMenu" onMouseDown={this.props.handleMouseDown} className={visibility}>
-//                 <div className='container-fluid'>
-//                 <MenuButton/>
-//                 <div className='col col-sm'>
-//                     <button type="button" className="btn btn-primary block" onMouseDown={}>Dress</button>
-//                 </div>
-//                     <div className='col col-sm'>
-//                         <button type="button" className="btn btn-primary block" onMouseDown={this.dress}>Bags</button>
-//                     </div>
-//
-//                 <button type="button" className="btn btn-primary block" onClick={this.dress}>Bags</button>
-//                 <button type="button" className="btn btn-primary block" onClick={this.dress}>Pants</button>
-//                 <button type="button" className="btn btn-primary block" onClick={this.dress}>Skirt</button>
-//                 <button type="button" className="btn btn-primary block" onClick={this.dress}>Jacket</button>
-//                 <div className="btn-group-vertical" role="group" aria-label="Basic example">
-//                     <button type="button" className="btn btn-secondary">Left</button>
-//                     <button type="button" className="btn btn-secondary">Middle</button>
-//                     <button type="button" className="btn btn-secondary">Right</button>
-//                 </div>
-//                 <h2>Bags</h2>
-//                 <h2>Dress</h2>
-//                 <h2>Pants</h2>
-//                 <h2>Skirt</h2>
-//                 <h2>Jacket</h2>
-//                 </div>
-//             </div>
-//         )
-//         }
-// }
-const Menu =(props) =>{
-    const [visibility,setVisibility] = useState(false);
-    // let visibility = "hide";
-    const dispatch = useDispatch();
-    const visibility_m = useSelector(state => state.visibility_m);
-    // if(props.menuVisibility){
-    //     visibility = "show";
-    // }
-
     return(
         <div id="flyoutMenu" className={visibility_m}>
             <div className='container-fluid'>
